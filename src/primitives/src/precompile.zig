93aae9e7f83e0890da01fc35e17553d698bfbdf2
Thomas Coratger <thomas.coratger@gmail.com>
2023-08-16 16:43:42 +0200

    feat(precompile): add PrecompileError error

---
+++ src/primitives/src/precompile.zig
@@ -0,0 +1,11 @@
+pub const PrecompileError = error {
+    OutOfGas,
+    Blake2WrongLength,
+    Blake2WrongFinalIndicatorFlag,
+    ModexpExpOverflow,
+    ModexpBaseOverflow,
+    ModexpModOverflow,
+    Bn128FieldPointNotAMember,
+    Bn128AffineGFailedToCreate,
+    Bn128PairLength,
+};
