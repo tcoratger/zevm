6a273e10d292c2bce02db73d5f568794bbb2f816
Thomas Coratger <thomas.coratger@gmail.com>
2023-09-12 11:28:38 +0200

    feat(primitives): add B256 and B160 primitive structures with basic unit tests

---
+++ src/primitives/src/bits.zig
@@ -0,0 +1,77 @@
+const std = @import("std");
+
+/// zevm 256 bits type.
+pub const B256 = struct {
+    bytes: [32]u8,
+
+    pub fn zero() B256 {
+        return B256{ .bytes = [_]u8{0} ** 32 };
+    }
+
+    pub fn serialize(self: B256) ![]const u8 {
+        var slice = [_]u8{0} ** (2 + 2 * 32);
+        return Serialize.serialize_raw(&slice, &self.bytes);
+    }
+};
+
+/// zevm 256 bits type.
+pub const B160 = struct {
+    bytes: [20]u8,
+
+    pub fn from(fr: u64) B160 {
+        return B160{ .bytes = [20]u8{
+            0,                           0,                           0,                           0,                           0,                           0,                           0,                          0,                   0, 0, 0, 0,
+            @intCast((fr >> 56) & 0xFF), @intCast((fr >> 48) & 0xFF), @intCast((fr >> 40) & 0xFF), @intCast((fr >> 32) & 0xFF), @intCast((fr >> 24) & 0xFF), @intCast((fr >> 16) & 0xFF), @intCast((fr >> 8) & 0xFF), @intCast(fr & 0xFF),
+        } };
+    }
+};
+
+pub const Serialize = struct {
+    const CHARS = "0123456789abcdef";
+
+    pub fn to_hex_raw(v: []u8, bytes: []const u8, skip_leading_zero: bool) ![]const u8 {
+        std.debug.assert(v.len > 2 + bytes.len * 2);
+
+        v[0] = '0';
+        v[1] = 'x';
+
+        var idx: usize = 2;
+        var first_nibble: u8 = bytes[0] >> 4;
+
+        if (first_nibble != 0 or !skip_leading_zero) {
+            v[idx] = CHARS[@as(usize, first_nibble)];
+            idx += 1;
+        }
+
+        v[idx] = CHARS[@as(usize, bytes[0] & 0xf)];
+        idx += 1;
+
+        for (bytes[1..]) |byte| {
+            v[idx] = CHARS[@as(usize, byte >> 4)];
+            v[idx + 1] = CHARS[@as(usize, byte & 0xf)];
+            idx += 2;
+        }
+
+        return v[0..idx];
+    }
+
+    /// Serializes a slice of bytes.
+    pub fn serialize_raw(slice: []u8, bytes: []const u8) ![]const u8 {
+        if (bytes.len == 0) {
+            return "0x";
+        } else {
+            return Serialize.to_hex_raw(slice, bytes, false);
+        }
+    }
+};
+
+test "B256: zero function" {
+    try std.testing.expectEqual(B256.zero(), B256{ .bytes = [_]u8{0} ** 32 });
+}
+
+test "B160: from u64 function" {
+    try std.testing.expectEqual(B160.from(34353535), B160{ .bytes = [20]u8{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 12, 49, 127 } });
+    try std.testing.expectEqual(B160.from(11111111), B160{ .bytes = [20]u8{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 169, 138, 199 } });
+    try std.testing.expectEqual(B160.from(0), B160{ .bytes = [20]u8{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } });
+    try std.testing.expectEqual(B160.from(std.math.maxInt(u64)), B160{ .bytes = [20]u8{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 255 } });
+}
