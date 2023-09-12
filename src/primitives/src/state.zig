ffb5612f28c8b2a549770a23dd6cebba646560c4
Thomas Coratger <thomas.coratger@gmail.com>
2023-09-13 00:16:44 +0200

    feat(primitives): add AccountInfo new function and unit tests

--- src/primitives/src/state.zig
+++ src/primitives/src/state.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const b256 = @import("./bits.zig");
+const bits = @import("./bits.zig");
 const bytecode = @import("./bytecode.zig");
 const constants = @import("./constants.zig");
 
@@ -11,7 +11,7 @@ pub const AccountInfo = struct {
     /// Account nonce.
     nonce: u64,
     /// code hash,
-    code_hash: b256.B256,
+    code_hash: bits.B256,
     /// code: if None, `code_by_hash` will be used to fetch it if code needs to be loaded from
     /// inside of revm.
     code: bytecode.Bytecode,
@@ -23,6 +23,10 @@ pub const AccountInfo = struct {
     pub fn eq(self: AccountInfo, other: AccountInfo) bool {
         return self.balance.toConst().eql(other.balance.toConst()) and self.nonce == other.nonce and self.code_hash.eql(other.code_hash);
     }
+
+    pub fn new(balance: std.math.big.int.Mutable, nonce: u64, code_hash: bits.B256, code: bytecode.Bytecode) AccountInfo {
+        return AccountInfo{ .balance = balance, .nonce = nonce, .code_hash = code_hash, .code = code };
+    }
 };
 
 test "AccountInfo: default function" {
@@ -32,11 +36,24 @@ test "AccountInfo: default function" {
     try std.testing.expectEqual(AccountInfo.default().balance.len, 1);
     try std.testing.expectEqual(AccountInfo.default().balance.positive, true);
     try std.testing.expectEqual(AccountInfo.default().nonce, 0);
-    try std.testing.expectEqual(AccountInfo.default().code_hash, b256.B256{ .bytes = [32]u8{ 197, 210, 70, 1, 134, 247, 35, 60, 146, 126, 125, 178, 220, 199, 3, 192, 229, 0, 182, 83, 202, 130, 39, 59, 123, 250, 216, 4, 93, 133, 164, 112 } });
+    try std.testing.expectEqual(AccountInfo.default().code_hash, bits.B256{ .bytes = [32]u8{ 197, 210, 70, 1, 134, 247, 35, 60, 146, 126, 125, 178, 220, 199, 3, 192, 229, 0, 182, 83, 202, 130, 39, 59, 123, 250, 216, 4, 93, 133, 164, 112 } });
     try std.testing.expectEqualSlices(u8, AccountInfo.default().code.bytecode, buf[0..]);
     try std.testing.expectEqual(AccountInfo.default().code.state.Analysed.len, 0);
 }
 
 test "AccountInfo: eq function" {
     try std.testing.expectEqual(AccountInfo.eq(AccountInfo.default(), AccountInfo.default()), true);
+}
+
+test "AccountInfo: new function" {
+    var lb = [_]usize{ 0, 0, 0, 0 };
+    var buf: [1]u8 = .{0};
+    var accountInfo = AccountInfo.new(std.math.big.int.Mutable.init(&AccountInfo.limbs, 0), 0, constants.Constants.KECCAK_EMPTY, bytecode.Bytecode.new());
+    try std.testing.expectEqualSlices(usize, accountInfo.balance.limbs, lb[0..]);
+    try std.testing.expectEqual(accountInfo.balance.len, 1);
+    try std.testing.expectEqual(accountInfo.balance.positive, true);
+    try std.testing.expectEqual(accountInfo.nonce, 0);
+    try std.testing.expectEqual(accountInfo.code_hash, bits.B256{ .bytes = [32]u8{ 197, 210, 70, 1, 134, 247, 35, 60, 146, 126, 125, 178, 220, 199, 3, 192, 229, 0, 182, 83, 202, 130, 39, 59, 123, 250, 216, 4, 93, 133, 164, 112 } });
+    try std.testing.expectEqualSlices(u8, accountInfo.code.bytecode, buf[0..]);
+    try std.testing.expectEqual(accountInfo.code.state.Analysed.len, 0);
 }
