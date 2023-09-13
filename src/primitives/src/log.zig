0c37c6b34864727fd015b2b942cf3a9fdc4c2487
MrPalixir <73360179+coratgerl@users.noreply.github.com>
2023-09-13 23:57:57 +0200

    feat: update log struct (#7)
    Co-authored-by: MrPalixir <73360179+MrPalixir@users.noreply.github.com>

--- src/primitives/src/log.zig
+++ src/primitives/src/log.zig
@@ -1,1 +1,8 @@
-pub const Log = struct { address: u64 };
+const std = @import("std");
+
+pub const Log = struct {
+    address: u64,
+    pub fn log(comptime message: []const u8) void {
+        std.debug.print("{s}\n", .{message});
+    }
+};
