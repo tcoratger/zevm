92a95dddaa4a1b45c4b5e17fc3d6b6a6db4916ba
Thomas Coratger <thomas.coratger@gmail.com>
2023-09-15 14:41:56 +0200

    feat(primitives): add Bytecode implementation, missing to_checked

--- src/primitives/src/bytecode.zig
+++ src/primitives/src/bytecode.zig
@@ -18,4 +18,52 @@ pub const Bytecode = struct {
         var buf: [1]u8 = .{0};
         return Bytecode{ .bytecode = buf[0..], .state = BytecodeState{ .Analysed = .{ .len = 0, .jump_map = JumpMap{} } } };
     }
+
+    pub fn new_raw(bytecode: []u8) Bytecode {
+        return Bytecode{ .bytecode = bytecode, .state = BytecodeState.Raw };
+    }
+
+    /// Create new checked bytecode
+    ///
+    /// # Safety
+    /// Bytecode need to end with STOP (0x00) opcode as checked bytecode assumes
+    /// that it is safe to iterate over bytecode without checking lengths
+    pub fn new_checked(bytecode: []u8, len: usize) Bytecode {
+        return Bytecode{
+            .bytecode = bytecode,
+            .state = BytecodeState{ .Checked = .{ .len = len } },
+        };
+    }
+
+    pub fn bytes(self: Bytecode) []u8 {
+        return self.bytecode;
+    }
+
+    pub fn original_bytes(self: Bytecode) []u8 {
+        return switch (self.state) {
+            .Raw => self.bytecode,
+            .Checked => |*item| self.bytecode[0..item.*.len],
+            .Analysed => |*item| self.bytecode[0..item.*.len],
+        };
+    }
+
+    pub fn state(self: Bytecode) BytecodeState {
+        return self.state;
+    }
+
+    pub fn is_empty(self: Bytecode) bool {
+        return switch (self.state) {
+            .Raw => self.bytecode.len == 0,
+            .Checked => |*item| item.*.len == 0,
+            .Analysed => |*item| item.*.len == 0,
+        };
+    }
+
+    pub fn get_len(self: Bytecode) usize {
+        return switch (self.state) {
+            .Raw => self.bytecode.len,
+            .Checked => |*item| item.*.len,
+            .Analysed => |*item| item.*.len,
+        };
+    }
 };
---
+++ src/primitives/src/bytecode_test.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+const bytecode = @import("./bytecode.zig");
+
+test "Bytecode: new_raw function" {
+    var buf: [5]u8 = .{ 1, 2, 3, 4, 5 };
+    try std.testing.expectEqual(bytecode.Bytecode.new_raw(buf[0..]).bytecode, buf[0..]);
+    try std.testing.expectEqual(bytecode.Bytecode.new_raw(buf[0..]).state, bytecode.BytecodeState.Raw);
+}
+
+test "Bytecode: new_checked function" {
+    var buf: [5]u8 = .{ 1, 2, 3, 4, 5 };
+    try std.testing.expectEqual(bytecode.Bytecode.new_checked(buf[0..], 10).bytecode, buf[0..]);
+    try std.testing.expectEqual(bytecode.Bytecode.new_checked(buf[0..], 10).state, bytecode.BytecodeState{ .Checked = .{ .len = 10 } });
+}
+
+test "Bytecode: bytes function" {
+    var buf: [5]u8 = .{ 1, 2, 3, 4, 5 };
+    var x = bytecode.Bytecode.new_checked(buf[0..], 10);
+    try std.testing.expectEqual(x.bytes(), buf[0..]);
+}
+
+test "Bytecode: original_bytes function" {
+    var buf: [5]u8 = .{ 1, 2, 3, 4, 5 };
+    try std.testing.expectEqual(bytecode.Bytecode.new_checked(buf[0..], 3).original_bytes(), buf[0..3]);
+    try std.testing.expectEqual(bytecode.Bytecode.new_raw(buf[0..]).original_bytes(), buf[0..]);
+}
+
+test "Bytecode: state function" {
+    var buf: [5]u8 = .{ 1, 2, 3, 4, 5 };
+    try std.testing.expectEqual(bytecode.Bytecode.state(bytecode.Bytecode.new_checked(buf[0..], 3)), bytecode.BytecodeState{ .Checked = .{ .len = 3 } });
+    try std.testing.expectEqual(bytecode.Bytecode.state(bytecode.Bytecode.new_raw(buf[0..])), bytecode.BytecodeState.Raw);
+}
+
+test "Bytecode: is_empty function" {
+    var buf: [5]u8 = .{ 1, 2, 3, 4, 5 };
+    try std.testing.expectEqual(bytecode.Bytecode.is_empty(bytecode.Bytecode.new_checked(buf[0..], 3)), false);
+    try std.testing.expectEqual(bytecode.Bytecode.is_empty(bytecode.Bytecode.new_raw(buf[0..0])), true);
+    try std.testing.expectEqual(bytecode.Bytecode.is_empty(bytecode.Bytecode.new_raw(buf[0..])), false);
+}
+
+test "Bytecode: len function" {
+    var buf: [5]u8 = .{ 1, 2, 3, 4, 5 };
+    try std.testing.expectEqual(bytecode.Bytecode.get_len(bytecode.Bytecode.new_checked(buf[0..], 3)), 3);
+    try std.testing.expectEqual(bytecode.Bytecode.get_len(bytecode.Bytecode.new_raw(buf[0..0])), 0);
+    try std.testing.expectEqual(bytecode.Bytecode.get_len(bytecode.Bytecode.new_raw(buf[0..])), 5);
+}
