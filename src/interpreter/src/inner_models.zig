f8e4d646e1a168e2c6c7834690c67a0a8f4e92ac
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2023-11-20 19:14:38 +0100

    tests: unit tests for `create_address` method in `CreateInputs` (#18)
    * wip
    
    * add unit tests for CreateInputs create_address method

--- src/interpreter/src/inner_models.zig
+++ src/interpreter/src/inner_models.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 const primitives = @import("primitives");
 
+const B160 = primitives.B160;
+
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
+
 /// Inputs for a call.
 pub const CallInputs = struct {
     /// The target of the call.
@@ -34,22 +39,23 @@ pub const CreateInputs = struct {
 
     /// Returns the address that this create call will create.
     pub fn create_address(
-        self: *Self,
+        self: *const Self,
         nonce: u64,
         allocator: std.mem.Allocator,
-    ) [20]u8 {
+    ) ![20]u8 {
+        var end: usize = 20;
         return switch (self.scheme) {
-            .Create => primitives.Utils.create_address(
-                primitives.B160.from_slice(self.caller[0..]),
+            .Create => (try primitives.Utils.create_address(
+                primitives.B160.from_slice(self.caller[0..end]),
                 nonce,
                 allocator,
-            ),
-            .Create2 => |*scheme| primitives.Utils.create2_address(
+            )).bytes,
+            .Create2 => |*scheme| (try primitives.Utils.create2_address(
                 primitives.B160.from_slice(self.caller[0..]),
-                primitives.B256.from_slice(&self.init_code),
+                primitives.B256.from_slice(self.init_code),
                 scheme.*.salt,
                 allocator,
-            ),
+            )).bytes,
         };
     }
 
@@ -138,3 +144,35 @@ pub const SelfDestructResult = struct {
     is_cold: bool,
     previously_destroyed: bool,
 };
+
+test "CreateInputs: create_address function with Create scheme" {
+    var tmp = [3]u8{ 1, 2, 3 };
+    const create_inputs: CreateInputs = .{
+        .caller = B160.from(18_446_744_073_709_551_615).bytes,
+        .scheme = .Create,
+        .value = 10,
+        .init_code = &tmp,
+        .gas_limit = 4,
+    };
+
+    try expectEqual(
+        [20]u8{ 4, 1, 133, 88, 123, 80, 98, 157, 3, 48, 181, 126, 60, 186, 109, 109, 136, 77, 127, 229 },
+        try create_inputs.create_address(2, std.testing.allocator),
+    );
+}
+
+test "CreateInputs: create_address function with Create2 scheme" {
+    var tmp = [32]u8{ 121, 72, 47, 147, 234, 13, 113, 78, 41, 51, 102, 50, 41, 34, 150, 42, 243, 142, 205, 217, 92, 255, 100, 131, 85, 193, 175, 75, 64, 167, 139, 50 };
+    const create_inputs: CreateInputs = .{
+        .caller = B160.from(18_446_744_073_709_551_615).bytes,
+        .scheme = .{ .Create2 = .{ .salt = 10000000000000000000000000000000 } },
+        .value = 10,
+        .init_code = &tmp,
+        .gas_limit = 4,
+    };
+
+    try expectEqual(
+        [20]u8{ 21, 108, 197, 97, 104, 190, 154, 181, 81, 131, 139, 5, 178, 141, 203, 240, 157, 66, 125, 96 },
+        try create_inputs.create_address(2, std.testing.allocator),
+    );
+}
