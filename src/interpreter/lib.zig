ef32b80f47f09558def9bbbb688fb94a18840df9
coratgerl <--global>
2023-11-20 20:44:46 +0100

    fix: remove comment

--- src/interpreter/lib.zig
+++ src/interpreter/lib.zig
@@ -1,5 +1,5 @@
 pub const interpreter = struct {
-    // pub usingnamespace @import("src/gas_calc.zig");
+    pub usingnamespace @import("src/gas_calc.zig");
     pub usingnamespace @import("src/gas.zig");
     pub usingnamespace @import("src/inner_models.zig");
     pub usingnamespace @import("src/instruction_result.zig");
--- src/vm/vm.zig
+++ src/vm/vm.zig
@@ -1,46 +1,46 @@
-const block = @import("./block.zig");
-const block_header = @import("./blockHeader.zig");
-const consensus = @import("./consensus.zig");
-const chain = @import("./chain.zig");
-const state = @import("./state.zig");
+// const block = @import("./block.zig");
+// const block_header = @import("./blockHeader.zig");
+// const consensus = @import("./consensus.zig");
+// const chain = @import("./chain.zig");
+// const state = @import("./state.zig");
 
-pub const VM = struct {
-    block_class: ?block.Block = null,
-    block: ?block.Block = null,
-    consensus: ?consensus.Consensus = null,
-    extra_data_max_bytes: u8 = 32,
-    fork: ?[]const u8 = null,
-    chaindb: chain.ChainDatabase,
-    state_class: ?state.State = null,
-    state: ?state.State = null,
+// pub const VM = struct {
+//     block_class: ?block.Block = null,
+//     block: ?block.Block = null,
+//     consensus: ?consensus.Consensus = null,
+//     extra_data_max_bytes: u8 = 32,
+//     fork: ?[]const u8 = null,
+//     chaindb: chain.ChainDatabase,
+//     state_class: ?state.State = null,
+//     state: ?state.State = null,
 
-    initial_header: block_header.BlockHeader,
-    chain_context: chain.ChainContext,
-    consensus_context: consensus.ConsensusContext,
+//     initial_header: block_header.BlockHeader,
+//     chain_context: chain.ChainContext,
+//     consensus_context: consensus.ConsensusContext,
 
-    pub fn init(_header: block_header.BlockHeader, _chaindb: chain.ChainDatabase, _chain_context: chain.ChainContext, _consensus_context: consensus.ConsensusContext) VM {
-        return VM{
-            .initial_header = _header,
-            .chaindb = _chaindb,
-            .chain_context = _chain_context,
-            .consensus_context = _consensus_context,
-        };
-    }
+//     pub fn init(_header: block_header.BlockHeader, _chaindb: chain.ChainDatabase, _chain_context: chain.ChainContext, _consensus_context: consensus.ConsensusContext) VM {
+//         return VM{
+//             .initial_header = _header,
+//             .chaindb = _chaindb,
+//             .chain_context = _chain_context,
+//             .consensus_context = _consensus_context,
+//         };
+//     }
 
-    pub fn get_header(self: VM) block_header.BlockHeader {
-        return if (self.block) |b| b.header else self.initial_header;
-    }
+//     pub fn get_header(self: VM) block_header.BlockHeader {
+//         return if (self.block) |b| b.header else self.initial_header;
+//     }
 
-    pub fn get_block(self: VM) block.Block {
-        if (self.block) |b| {
-            return b;
-        } else {
-            const bc = self.get_block_class();
-            _ = bc;
-        }
-    }
+//     pub fn get_block(self: VM) block.Block {
+//         if (self.block) |b| {
+//             return b;
+//         } else {
+//             const bc = self.get_block_class();
+//             _ = bc;
+//         }
+//     }
 
-    pub fn get_block_class(self: VM) !block.Block {
-        return if (self.block_class) |b| b else error.No_Block_Class_Set;
-    }
-};
+//     pub fn get_block_class(self: VM) !block.Block {
+//         return if (self.block_class) |b| b else error.No_Block_Class_Set;
+//     }
+// };
